94b256150b8b39731b7052a6f268425ab2b19a13
minung.han <hmu332233@gmail.com>
2018-12-27 00:44:40 +0900

    [Add] jbnu_info - parseHtml

--- jbnu_info/index.js
+++ jbnu_info/index.js
@@ -1,7 +1,7 @@
 const request = require('request');
 const cheerio = require('cheerio');
 
-const url = 'https://www.jbnu.ac.kr/kor/?menuID=10&pid=254';
+const url = 'https://www.jbnu.ac.kr/kor/?menuID=10&pid=256';
 
 request({
   uri: url,
@@ -10,18 +10,23 @@ request({
     'Accept-Charset': 'utf-8'
   }
 }, function (err, res, body) {
-  const $ = cheerio.load(body, {
+  const infos = parseHtml(body);
+  console.log(infos);
+});
+
+function parseHtml(html) {
+  const $ = cheerio.load(html, {
     decodeEntities: false
   });
 
   const rows = $('#print_area > div:nth-child(3) > div.subtable.orgtable > table > tbody > tr');
-
-  rows.each(function () {
+  const infos = rows.map(function () {
     const colums = $(this).find('td');
     const name = $(colums[0]).text().trim();
     const location = $(colums[1]).text().trim();
     const number = $(colums[2]).text().trim().replace(/\s/g, '');
+    return { name, location, number};
+  }).get();
 
-    console.log(name, location, number);
-  });
-});
+  return infos;
+}
