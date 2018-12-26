1c64ff5628dc47fe6f9350aa3b21001efe18121a
minung.han <hmu332233@gmail.com>
2018-12-27 01:18:08 +0900

    [Modify] jbnu_info - request async로 수정

--- jbnu_info/index.js
+++ jbnu_info/index.js
@@ -1,18 +1,36 @@
-const request = require('request');
 const cheerio = require('cheerio');
+const { promisify } = require('util');
+const { get } = require('request');
+const [ getPm ] = [get].map(promisify);
+
+const pidList = [
+  254,
+  256,
+  269,
+  286,
+  336,
+  320,
+  314,
+  247,
+  302,
+  326,
+  299,
+  318,
+  310,
+  1686,
+  1776,
+  1778
+];
 
 const url = 'https://www.jbnu.ac.kr/kor/?menuID=10&pid=256';
 
-request({
-  uri: url,
-  method: 'GET',
-  headers: {
-    'Accept-Charset': 'utf-8'
-  }
-}, function (err, res, body) {
+(async function() {
+  const { body } = await getPm(url);
   const infos = parseHtml(body);
+
   console.log(infos);
-});
+})();
+
 
 function parseHtml(html) {
   const $ = cheerio.load(html, {
@@ -20,12 +38,12 @@ function parseHtml(html) {
   });
 
   const rows = $('#print_area > div:nth-child(3) > div.subtable.orgtable > table > tbody > tr');
-  const infos = rows.map(function () {
+  const infos = rows.map(function() {
     const colums = $(this).find('td');
     const name = $(colums[0]).text().trim();
     const location = $(colums[1]).text().trim();
     const number = $(colums[2]).text().trim().replace(/\s/g, '');
-    return { name, location, number};
+    return { name, location, number };
   }).get();
 
   return infos;
